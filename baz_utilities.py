1c22dab48706a3f147ffbf9227990ff4a5a3a77f
Bruno AZRIA <bruno.azria@gmail.com>
2017-12-14 10:57:32 +0100

    clean useless comments and code

---
+++ baz_utilities.py
@@ -0,0 +1,123 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import json
+
+unsupported_format_error_message = "Sorry, we handle only the following formats: 'json','str'(default)."
+
+
+def get(filename, format = 'str', splitlines = False):
+    """
+    Return filename content in specified format. Manage exceptions: file presence, access mode and other.
+    """
+    try:
+        with open(filename, 'r', encoding ='ISO-8859-1') as input_file:
+            if format == 'json':
+                return json.load(input_file)
+            elif format == 'str':
+                if splitlines:
+                    return input_file.read().splitlines()
+                else:
+                    return input_file.read()
+            else:
+                raise Exception(unsupported_format_error_message)
+    except FileNotFoundError:
+        print('File not found: %s' % (filename))
+    except PermissionError:
+        print('File not in read mode: %s' % (filename))
+    except Exception as exception_other:
+        print('File error: %s' % (filename))
+        print(exception_other)
+
+
+def put(content, filename, format = 'str', indent = 0):
+    """
+    Write content into filename in specified format. Manage exception.
+    """
+    try:
+        with open(filename, 'w') as output_file:
+            # if content is not empty, write it in the file
+            if content:
+                if format == 'json':
+                    # CAUTION: setting ensure_ascii = True prevents from writing properly accents
+                    json.dump(obj = content, fp = output_file, ensure_ascii = False, indent = indent)
+                elif format == 'str':
+                    output_file.write(str(content))
+                else:
+                    raise Exception(unsupported_format_error_message)
+            # if content is empty, write an empty file to ease immediate visual check
+            else:
+                pass
+    except Exception as exception_other:
+        print('File error: %s' % (filename))
+
+
+def put_debug(variable_name, variable, type_id = False):
+    """
+    Print variable name, value, type and id for debugging purposes.
+    """
+    print('------------------------------')
+    print('variable name :', variable_name)
+    if type_id:
+        print('variable type :', type(variable))
+        print('variable id   :', id(variable))
+    print('variable value:', variable)
+    print('------------------------------')
+
+
+def unquoted(string):
+    """
+    Remove as many pairs of enclosing quotes (simple or double) as possible.
+    """
+    try:
+        if (string.startswith('"') and string.endswith('"')):
+            return unquoted(string.lstrip('"').rstrip('"'))
+        elif (string.startswith("'") and string.endswith("'")):
+            return unquoted(string.lstrip("'").rstrip("'"))
+        else:
+            return(string)
+    except:
+        return(string)
+
+
+def test_unquoted():
+    """
+    Test function unquoted.
+    """
+    variable = "test 00: string variable"
+    print(variable)
+    print(unquoted(variable))
+    print(unquoted('test 01: simple quotes'))
+    print(unquoted("test 02: double quotes"))
+    print(unquoted("'test 03: double, simple quotes'"))
+    print(unquoted('"test 04: simple, double quotes"'))
+    print(unquoted("""""test 05: 5 double quotes"""""))
+    print(unquoted('''''test 06: 5 simple quotes'''''))
+    print(unquoted("'"'"test 07: 5 alternate double & simple quotes"'"'"))
+    print(unquoted('"'"'test 08: 5 alternate double & simple quotes'"'"'))
+    print(unquoted('123'))
+    print(unquoted("123"))
+    print(unquoted('''123'''))
+    print(unquoted("""123"""))
+
+
+def secured_str(string):
+    """
+    From the argument string, returns a string that can be used as a string without any further precaution regarding presence of: simple quotes, double quotes, apostrophes, etc.
+    tbd baz
+    """
+    pass
+
+
+if __name__ == '__main__':
+    """
+    When run as a script, execute module tests.
+    """
+    test_unquoted()
+#    test_get()
+#    test_put()
+#    test_put_debug()
+# tests for unquoted
+#    put_debug('content', get('baz_utilities_testdata_in_00.txt', format = 'mp9'))
+#    put('test data', 'baz_utilities_testdata_out_json.txt', format = 'json', indent = 4)
+#    put('test data', 'baz_utilities_testdata_out_str.txt', format = 'str')
