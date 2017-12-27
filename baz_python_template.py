8eb337c0bfca44c0b61c8297b68f201cbb2570e0
Bruno AZRIA <bruno.azria@gmail.com>
2017-12-27 15:14:00 +0100

    initial commit

---
+++ baz_python_template.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+def hello_world():
+    # indentation with whitespace not tab
+    """
+    Docstring feature
+    Print "Hello world!" on standard output.  No argument required.
+    """
+    print()
+    print('''Print "Hello world!" on standard output.  No argument required.''')
+    print()
+
+if __name__ == '__main__':
+    # execute only if run as a script
+    print()
+    print('''OK, Dude...''')
+    print()
