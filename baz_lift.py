60b761e5c8c8a44d7b9d7c4f1654f0237c514a2a
Bruno AZRIA <bruno.azria@gmail.com>
2018-02-26 15:38:05 +0100

    initial commit, wip

---
+++ baz_lift.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+def next_floor_to_serve(current_floor, floors_to_serve, lift_direction):
+    pass
+
+def run_lift(lowest_floor, highest_floor, floors_to_serve=[]):
+    """
+    tbd
+    """
+    print('lowest_floor:', lowest_floor)
+    print('highest_floor:', highest_floor)
+    while floors_to_serve:
+        print('floors_to_serve:', floors_to_serve)
+        floors_to_serve = list(set(floors_to_serve))
+        floors_to_serve.pop()
+# if lift_is_going_up, next_floor_to_serve = 
+# else next_floor_to_serve = 
+
+
+if __name__ == '__main__':
+    lowest_floor = 0
+    highest_floor= 4
+    run_lift(lowest_floor  = lowest_floor,
+             highest_floor = highest_floor,
+             floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4],
+             )
