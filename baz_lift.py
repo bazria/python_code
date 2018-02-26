07337289f5f33554f0ccdb721e5921a6bdaa03e5
Bruno AZRIA <bruno.azria@gmail.com>
2018-02-26 16:10:34 +0100

    dev, wip

--- baz_lift.py
+++ baz_lift.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+def go
 def next_floor_to_serve(current_floor, floors_to_serve, lift_direction):
     pass
 
@@ -21,7 +22,8 @@ def run_lift(lowest_floor, highest_floor, floors_to_serve=[]):
 if __name__ == '__main__':
     lowest_floor = 0
     highest_floor= 4
+    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4]
     run_lift(lowest_floor  = lowest_floor,
              highest_floor = highest_floor,
-             floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4],
+             floors_to_serve = floors_to_serve,
              )
