7f16b0cab8f0f7a1c74e8c0a3456d56b019b273f
Bruno AZRIA <bruno.azria@gmail.com>
2018-02-27 01:09:49 +0100

    dev, wip

--- baz_lift.py
+++ baz_lift.py
@@ -6,39 +6,66 @@
 # tbd use wait 1 second, add sort or sort reverse to find next stop according to direction
 
 
-def put_information(current_floor, way, floors_to_serve):
-    print('Lift at floor', current_floor, 'going', way, 'serving floors', floors_to_serve)
+def put_information(current_floor, way, floors_to_serve=[], mode='operation'):
+    print('Lift stopped at floor:', current_floor, 'in mode:', mode, 'going:', way, 'serving floors:', floors_to_serve)
 
 
-def serve_next_floor(current_floor, floors_to_serve, lift_direction):
-    pass
+def put_information_start(lowest_floor, highest_floor, mode='operation'):
+    print()
+    print('Hello, lift entering operation mode ...')
+    print('Lowest_floor: ', lowest_floor)
+    print('Highest_floor:', highest_floor)
+    print('At each stop, enter floors to serve separated by commas and/or STOP, and terminate entry by Enter.')
+    print()
 
 
-def run_lift(lowest_floor, highest_floor, floors_to_serve=[]):
+def put_information_stop(lowest_floor, highest_floor):
+    print()
+    print('Hello, lift exiting operation mode and returning to floor 0 ...')
+    print()
+
+
+def serve_next_floor(current_floor, floors_to_serve, way, lowest_floor, highest_floor):
+    floors_to_serve = [x for x in floors_to_serve if isinstance(x, int)]
+    floors_to_serve = sorted(list(set(floors_to_serve)))
+    put_information(current_floor  = current_floor,
+                    way            = way,
+                    floors_to_serve=floors_to_serve)
+# if lift_is_going_up, next_floor_to_serve = 
+# else next_floor_to_serve = 
+#    isinstance (integer) or str
+
+
+def run_lift(lowest_floor, highest_floor, floors_to_serve=[], mode='operation'):
     """
     tbd
     """
-    print()
-    print('Hello, lift entering operating mode ...')
-    print('lowest_floor:', lowest_floor)
-    print('highest_floor:', highest_floor)
-    print('At each stop, enter floors to serve separated by commas and/or STOP, and terminate entry by Enter')
-    print()
-    current_floor = 0
-    is_operating = True
-    while is_operating:
-        floors_to_serve = sorted(list(set(floors_to_serve)))
-        print('floors_to_serve:', floors_to_serve)
-        floors_to_serve.pop()
-# if lift_is_going_up, next_floor_to_serve = 
-# else next_floor_to_serve = 
+    lowest_floor    = lowest_floor
+    highest_floor   = highest_floor
+    floors_to_serve = floors_to_serve
+    mode            = mode
+    current_floor   = 0
+    way             = 'upwards'
+    put_information_start(lowest_floor, highest_floor)
+    while mode == 'operation' or mode == 'simulation':
+        put_information(current_floor, way, floors_to_serve, mode)
+        current_floor, floors_to_serve, way = serve_next_floor(current_floor, floors_to_serve, way, lowest_floor, highest_floor)
+        floors_to_serve = updated_floors_to_serve
 
 
 if __name__ == '__main__':
+    mode = 'simulation'
     lowest_floor    = 0
     highest_floor   = 4
     floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4]
     run_lift(lowest_floor    = lowest_floor,
              highest_floor   = highest_floor,
              floors_to_serve = floors_to_serve,
+             mode            = mode,
+             )
+    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 'STOP', 2, 3, 4]
+    run_lift(lowest_floor    = lowest_floor,
+             highest_floor   = highest_floor,
+             floors_to_serve = floors_to_serve,
+             mode            = mode,
              )
