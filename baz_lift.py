05fb4134b53b05b4fb857e70bf65d0c763d83571
Bruno AZRIA <bruno.azria@gmail.com>
2018-02-27 17:39:00 +0100

    dev, wip

--- baz_lift.py
+++ baz_lift.py
@@ -1,8 +1,10 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+# EXERCISE
 # Votre client vous propose de développer un software pour faire fonctionner un ascenseur. Le hardware de cet ascenseur possède 2 commandes : goUp() et goDown().  Ecrire dans le langage de votre choix le software permettant de faire fonctionner les ascenseurs équipés de ce hardware. 
 
+# ANSWER
 # Hypothèses :
 # 1. goUp() (respectivement goDown() ) déclenche sur l'ascenseur :
 #   - la fermeture des portes,
@@ -13,33 +15,45 @@
 # tbd use wait 1 second, add sort or sort reverse to find next stop according to direction
 
 
-def put_information(current_floor, way, floors_to_serve=[], mode='operation'):
+def put_information(current_floor, way, floors_to_serve, mode):
     print('Lift stopped at floor:', current_floor, 'in mode:', mode, 'going:', way, 'serving floors:', floors_to_serve)
 
 
-def put_information_start(lowest_floor, highest_floor, mode='operation'):
+def put_information_start(lowest_floor, highest_floor, mode):
     print()
-    print('Hello, lift entering operation mode ...')
-    print('Lowest_floor: ', lowest_floor)
-    print('Highest_floor:', highest_floor)
-    print('At each stop, enter floors to serve separated by commas and/or STOP, and terminate entry by Enter.')
+    print('Hello...')
+    print('Lift starting in mode:', mode)
+    print('Lowest_floor         :', lowest_floor)
+    print('Highest_floor        :', highest_floor)
     print()
 
 
-def put_information_stop(lowest_floor, highest_floor):
+def put_information_stop(lowest_floor, highest_floor, mode):
     print()
     print('Hello, lift exiting operation mode and returning to floor 0 ...')
     print()
 
 
-def move_lift(current_floor, to_floor=0):
-    pass
+def updated_floors_to_serve(floors_to_serve):
+#    print('At each stop, enter floors to serve separated by commas and/or STOP, and terminate entry by Enter.')
+    return floors_to_serve
+
+
+def move_lift(current_floor, target_floor):
+    if target_floor > current_floor:
+        for floor in range(target_floor - current_floor):
+            goUp()
+    elif target_floor < current_floor:
+        for floor in range(target_floor - current_floor):
+            goDown()
+    else:
+        pass
 
 
 def serve_next_floor(current_floor, floors_to_serve, way, lowest_floor, highest_floor):
     if 'STOP' in floors_to_serve:
         mode =  'stop'
-        go_to(current_floor, floor=0)
+        move_lift(current_floor, target_floor=0)
     else:
         floors_to_serve = [x for x in floors_to_serve if isinstance(x, int)]
         floors_to_serve = sorted(list(set(floors_to_serve)))
@@ -51,7 +65,7 @@ def serve_next_floor(current_floor, floors_to_serve, way, lowest_floor, highest_
 #    isinstance (integer) or str
 
 
-def run_lift(lowest_floor, highest_floor, floors_to_serve=[], mode='operation'):
+def run_lift(lowest_floor, highest_floor, floors_to_serve, mode):
     """
     tbd
     """
@@ -61,25 +75,35 @@ def run_lift(lowest_floor, highest_floor, floors_to_serve=[], mode='operation'):
     mode            = mode
     current_floor   = 0
     way             = 'upwards'
-    put_information_start(lowest_floor, highest_floor)
+    put_information_start(lowest_floor, highest_floor, mode)
     while mode == 'operation' or mode == 'simulation':
         current_floor, floors_to_serve, way = serve_next_floor(current_floor, floors_to_serve, way, lowest_floor, highest_floor)
         floors_to_serve = updated_floors_to_serve
 
 
 if __name__ == '__main__':
-    mode = 'simulation'
+# when called directly, run simulation mode
+    mode            = 'simulation'
     lowest_floor    = 0
     highest_floor   = 4
-    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4]
+# run simple test with initial list of floors
+    floors_to_serve = [-1, 5, 0, 4]
     run_lift(lowest_floor    = lowest_floor,
              highest_floor   = highest_floor,
              floors_to_serve = floors_to_serve,
              mode            = mode,
              )
-    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 'STOP', 2, 3, 4]
-    run_lift(lowest_floor    = lowest_floor,
-             highest_floor   = highest_floor,
-             floors_to_serve = floors_to_serve,
-             mode            = mode,
-             )
+## run test with initial list of floors
+#    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 2, 3, 4]
+#    run_lift(lowest_floor    = lowest_floor,
+#             highest_floor   = highest_floor,
+#             floors_to_serve = floors_to_serve,
+#             mode            = mode,
+#             )
+## run test with initial list of floors + STOP order
+#    floors_to_serve = [-1, 5, 0, 4, 2, 3, 0, 1, -9, 10, 'STOP', 2, 3, 4]
+#    run_lift(lowest_floor    = lowest_floor,
+#             highest_floor   = highest_floor,
+#             floors_to_serve = floors_to_serve,
+#             mode            = mode,
+#             )
